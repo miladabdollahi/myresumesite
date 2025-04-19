05eff1a544fee6ad64f485c5c32b1ecb2034cb6d
milad <miladabdollahi1397@gmail.com>
2025-04-19 21:14:59 +0330

    Add sitemap.xml and robots.txt and seo tags.

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -17,6 +17,25 @@ const AppLayout: React.FC<Props> = ({ children, title }) => {
     <>
       <Head>
         <title>{pageTitle}</title>
+        <meta
+          name="description"
+          content="من میلاد عبدالهی هستم، توسعه‌دهنده نرم‌افزار با تخصص در ERP، اتوماسیون کسب‌وکار، و برنامه‌نویس وب با پایتون، Django و React."
+        />
+        <meta
+          name="keywords"
+          content="میلاد عبدالهی, برنامه‌نویس, توسعه‌دهنده وب, ERP, نرم‌افزار, اتوماسیون, Python, Django, React, Next.js, سایت شخصی, توسعه دهنده Odoo"
+        />
+
+        <meta property="og:title" content="میلاد عبدالهی | برنامه‌نویس و توسعه‌دهنده نرم‌افزار" />
+        <meta
+          property="og:description"
+          content="معرفی من و مهارت‌هام در حوزه توسعه نرم‌افزار و اتوماسیون کسب‌وکار. مشاهده نمونه‌کارها و خدمات مشاوره‌ای."
+        />
+        <meta property="og:url" content="https://milad-abdollahi.ir" />
+        <meta property="og:type" content="website" />
+
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://milad-abdollahi.ir" />
       </Head>
       <div className="flex min-h-screen flex-col">
         <ScrollToTop
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import AboutSection from '@/components/partials/AboutSection';
-import BlogSection from '@/components/partials/BlogSection';
+// import BlogSection from '@/components/partials/BlogSection';
 import ContactSection from '@/components/partials/ContactSection';
 import ExperienceSection from '@/components/partials/ExperienceSection';
 import HeroSection from '@/components/partials/HeroSection';
-import RecentWorkSection from '@/components/partials/RecentWorkSection';
+// import RecentWorkSection from '@/components/partials/RecentWorkSection';
 import ServiceSection from '@/components/partials/ServiceSection';
-import TestimonialSection from '@/components/partials/TestimonialSection';
+// import TestimonialSection from '@/components/partials/TestimonialSection';
 import AppLayout from '@/layouts/AppLayout';
-import { api } from '@/lib/api';
+// import { api } from '@/lib/api';
 import type { GetServerSideProps, NextPage } from 'next';
 
 // type Props = {
